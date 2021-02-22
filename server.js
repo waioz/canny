77ed2cce91819100b9d43da19e9f521399fce09f
ak <ak@waioz.com>
2021-02-22 19:03:07 +0530

    port change - karthick

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/user-app'));
 
-app.get('/*', (req, res) =>
+app.get('/', (req, res) =>
     res.sendFile('index.html', { root: 'dist/user-app/' }),
 );
 
