7f9da6c466e601c762fb5fd494388e3c3dc7c152
ak <ak@waioz.com>
2021-02-22 19:53:17 +0530

    port change - karthick

--- server.js
+++ server.js
@@ -2,13 +2,23 @@
 const express = require('express');
 
 const app = express();
+const cwd = process.cwd();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/user-app'));
 
-app.get('/', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/user-app/' }),
-);
+console.log("run")
+app.get('/', (req, res) => {
+    console.log("req")
+    try {
+        console.log("try")
+        var text = await readFile(cwd + '/dist/user-app/index.html', 'utf8')
+        return res.send(text)
+    } catch (error) {
+        console.log("cache")
+        return res.send(error.message)
+    }
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.HTTP_PORT || 8080);
